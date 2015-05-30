93947e061ce7cea771b4d21a1025b37e61d5255a
Tim Smith <tim@cozy.co>
2015-05-30 13:26:49 -0700

    Update version library

--- lib/sensu-plugins-logs.rb
+++ lib/sensu-plugins-logs.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-logs/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsLogs
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
