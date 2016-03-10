ff0168fb7f92b3a4fd6c2315097b3be91536393e
Brandon Reuter <Brandon.Reuter@target.com>
2016-03-10 16:06:16 -0600

    check-log add features, refactor

--- bin/check-log.rb
+++ bin/check-log.rb
@@ -3,8 +3,9 @@
 #   check-log
 #
 # DESCRIPTION:
-#   This plugin checks a log file for a regular expression, skipping lines
-#   that have already been read, like Nagios's check_log. However, instead
+#   This plugin checks a log file, or collection of log files matching
+#   a pattern, for a regular expression, skipping lines that have
+#   already been read, like Nagios's check_log. However, instead
 #   of making a backup copy of the whole log file (very slow with large
 #   logs), it stores the number of bytes read, and seeks to that position
 #   next time.
@@ -22,8 +23,12 @@
 # USAGE:
 #   #YELLOW
 #
-# NOTES:
+# EXAMPLES:
+#   Returns matched lines, searches all files containing "logfile",
+#   ignores all .gz files, searches for "ERROR" in each line,
+#   returns a max of 4 matched lines in output
 #
+#   check-log.rb -r -F "logfile" -q "ERROR" -X "^.+\.gz$" -t 4
 # LICENSE:
 #   Copyright 2011 Sonian, Inc <chefs@sonian.net>
 #   Released under the same terms as Sensu (the MIT license); see LICENSE
@@ -59,9 +64,16 @@ class CheckLog < Sensu::Plugin::Check::CLI
          long: '--pattern PAT'
 
   option :exclude,
-         description: 'Pattern to exclude from matching',
+         description: 'Pattern to exclude from log line matching',
          short: '-E PAT',
          long: '--exclude PAT',
+         proc: proc { |s| Regexp.compile s },
+         default: /(?!)/
+
+  option :exclude_file,
+         descprtion: 'Pattern to exclude from file matching',
+         short: '-X PAT',
+         long: '--exclude_file PAT',
          proc: proc { |s| Regexp.compile s },
          default: /(?!)/
 
@@ -107,40 +119,26 @@ class CheckLog < Sensu::Plugin::Check::CLI
          boolean: true,
          default: false
 
+  option :return_threshold,
+         desciption: 'Set maximum number of returned lines for each file, if -r specified',
+         short: '-t N',
+         long: '--return_t N',
+         proc: proc(&:to_i),
+         default: 50
+
   def run
     unknown 'No log file specified' unless config[:log_file] || config[:file_pattern]
     unknown 'No pattern specified' unless config[:pattern]
-    file_list = []
-    file_list << config[:log_file] if config[:log_file]
-    if config[:file_pattern]
-      dir_str = config[:file_pattern].slice(0, config[:file_pattern].to_s.rindex('/'))
-      file_pat = config[:file_pattern].slice((config[:file_pattern].to_s.rindex('/') + 1), config[:file_pattern].length)
-      Dir.foreach(dir_str) do |file|
-        if config[:case_insensitive]
-          file_list << "#{dir_str}/#{file}" if file.to_s.downcase.match(file_pat.downcase)
-        else
-          file_list << "#{dir_str}/#{file}" if file.to_s.match(file_pat) # rubocop:disable Style/IfInsideElse
-        end
-      end
-    end
-    n_warns_overall = 0
-    n_crits_overall = 0
-    error_overall = ''
-    file_list.each do |log_file|
-      begin
-        open_log log_file
-      rescue => e
-        unknown "Could not open log file: #{e}"
-      end
-      n_warns, n_crits, accumulative_error = search_log
-      n_warns_overall += n_warns
-      n_crits_overall += n_crits
 
-      if config[:return_content]
-        error_overall = accumulative_error
-      end
+    file_list = load_file_list
+    n_warns_overall, n_crits_overall, accumulative_error = search_files file_list
+
+    if config[:return_content]
+      error_overall = accumulative_error
     end
+
     message "#{n_warns_overall} warnings, #{n_crits_overall} criticals for pattern #{config[:pattern]}. #{error_overall}"
+
     if n_crits_overall > 0
       critical
     elsif n_warns_overall > 0
@@ -150,6 +148,51 @@ class CheckLog < Sensu::Plugin::Check::CLI
     end
   end
 
+  # Populates the file list based on log_file name or pattern provided
+  # Return: matched file or list of files, nil if none found
+  def load_file_list
+    file_list = []
+    file_list << config[:log_file] if config[:log_file]
+
+    if config[:file_pattern]
+      dir_str = config[:file_pattern].slice(0, config[:file_pattern].to_s.rindex('/'))
+      file_pat = config[:file_pattern].slice((config[:file_pattern].to_s.rindex('/') + 1), config[:file_pattern].length)
+      Dir.foreach(dir_str) do |file|
+        if config[:case_insensitive] && !config[:exclude_file]
+          file_list << "#{dir_str}/#{file}" if file.to_s.downcase.match(file_pat.downcase)
+        else
+          file_list << "#{dir_str}/#{file}" if file.to_s.match(file_pat)
+        end
+      end
+    end
+    file_list
+  end
+
+  # Processes list of log files, passes each to the search_log method
+  # Input: file_list, a list of one or more file names
+  # Return: total warnings, total criticals, and total error messages for the file list
+  def search_files(file_list)
+    n_warns_overall = 0
+    n_crits_overall = 0
+    overall_error = ''
+
+    file_list.each do |log_file|
+      begin
+        open_log log_file
+      rescue => e
+        unknown "Could not open log file: #{e}"
+      end
+      n_warns, n_crits, accumulative_error = search_log log_file
+      n_warns_overall += n_warns
+      n_crits_overall += n_crits
+      overall_error += accumulative_error
+    end
+
+    [n_warns_overall, n_crits_overall, overall_error]
+  end
+
+  # Opens the given log file, reads its state file to get the bytes to skip and inode number
+  # Input: a single log file
   def open_log(log_file)
     state_dir = config[:state_auto] || config[:state_dir]
 
@@ -161,53 +204,85 @@ class CheckLog < Sensu::Plugin::Check::CLI
            end
 
     @state_file = File.join(state_dir, File.expand_path(log_file).sub(/^([A-Z]):\//, '\1/'))
+
+    @inode = begin
+      File.open(@state_file) do |file|
+        file.readline.each_line(':').to_a[0].to_i
+      end
+    rescue
+      @log.stat.ino
+    end
+
     @bytes_to_skip = begin
       File.open(@state_file) do |file|
-        file.readline.to_i
+        file.readline.each_line(':').to_a[1].to_i
       end
     rescue
       0
     end
   end
 
-  def search_log
-    log_file_size = @log.stat.size
-    @bytes_to_skip = 0 if log_file_size < @bytes_to_skip
-    bytes_read = 0
+  # Checks for a match, if found it records the line message and severity (critical default)
+  # Input: a single log file, the match_data, and the text line matched
+  # Return: A count of warnings/criticals, and error string
+  def process_match(log_file, match, line)
     n_warns = 0
     n_crits = 0
-    accumulative_error = ''
+    error = ''
+
+    if match
+      error = "\n" + log_file + ': ' + line.slice(0, 250) unless config[:return_threshold] && $NR > config[:return_threshold]
+      if match[1]
+        if config[:crit] && match[1].to_i > config[:crit]
+          n_crits = 1
+        elsif config[:warn] && match[1].to_i > config[:warn]
+          n_warns = 1
+        end
+      else
+        if config[:only_warn]
+          n_warns = 1
+        else
+          n_crits = 1
+        end
+      end
+    end
+    [n_warns, n_crits, error]
+  end
+
+  # Searches the open log file for the provided pattern, skipping previously read bytes
+  # Input: a single log file
+  # Return: number of warnings found, number of criticals found, error string for all errors found
+  def search_log(log_file)
+    log_file_size = @log.stat.size
+    inode = @log.stat.ino
+    @bytes_to_skip = 0 if log_file_size < @bytes_to_skip || inode != @inode
+    bytes_read = 0
+    n_warns_overall = 0
+    n_crits_overall = 0
+    error_overall = ''
 
     @log.seek(@bytes_to_skip, File::SEEK_SET) if @bytes_to_skip > 0
     # #YELLOW
     @log.each_line do |line|
       bytes_read += line.bytesize
       if config[:case_insensitive]
-        m = line.downcase.match(config[:pattern].downcase) unless line.match(config[:exclude])
+        match = line.downcase.match(config[:pattern].downcase) unless line.match(config[:exclude])
       else
-        m = line.match(config[:pattern]) unless line.match(config[:exclude])
+        match = line.match(config[:pattern]) unless line.match(config[:exclude])
       end
-      if m
-        accumulative_error += "\n" + line.slice(0, 250)
-        if m[1]
-          if config[:crit] && m[1].to_i > config[:crit]
-            n_crits += 1
-          elsif config[:warn] && m[1].to_i > config[:warn]
-            n_warns += 1
-          end
-        else
-          if config[:only_warn] # rubocop:disable Style/IfInsideElse
-            n_warns += 1
-          else
-            n_crits += 1
-          end
-        end
-      end
+      n_warns, n_crits, error = process_match(log_file, match, line)
+      n_warns_overall += n_warns
+      n_crits_overall += n_crits
+      error_overall += error
     end
+
+    # Update state file with bytes read
     FileUtils.mkdir_p(File.dirname(@state_file))
     File.open(@state_file, 'w') do |file|
-      file.write(@bytes_to_skip + bytes_read)
+      file.write("#{inode}:#{@bytes_to_skip + bytes_read}")
     end
-    [n_warns, n_crits, accumulative_error]
+    [n_warns_overall, n_crits_overall, error_overall]
   end
+
+  private :load_file_list, :search_files, :open_log, :process_match, :search_log
 end
