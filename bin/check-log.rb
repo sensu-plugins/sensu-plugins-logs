5f18f96c2eabc82046290e1590270d6a1803d1fe
Norberto Lopes <nlopes.ml@gmail.com>
2016-08-05 15:52:02 +0100

    Write state file atomically
    If you have two different calls of this script on the same file, one of
    the runs may be at the point of "truncate" when writing the state
    file. If the other run is trying to read, that will raise and the
    default is rescue with 0. This means that your check will bounce between
    OK and critical/warning depending if these runs happen to stomp one
    another.

--- bin/check-log.rb
+++ bin/check-log.rb
@@ -169,6 +169,7 @@ class CheckLog < Sensu::Plugin::Check::CLI
     @state_file = File.join(state_dir, File.expand_path(log_file).sub(/^([A-Z]):\//, '\1/'))
     @bytes_to_skip = begin
       File.open(@state_file) do |file|
+        file.flock(File::LOCK_SH)
         file.readline.to_i
       end
     rescue
@@ -212,7 +213,8 @@ class CheckLog < Sensu::Plugin::Check::CLI
       end
     end
     FileUtils.mkdir_p(File.dirname(@state_file))
-    File.open(@state_file, 'w') do |file|
+    File.open(@state_file, File::RDWR | File::CREAT, 0644) do |file|
+      file.flock(File::LOCK_EX)
       file.write(@bytes_to_skip + bytes_read)
     end
     [n_warns, n_crits, accumulative_error]
