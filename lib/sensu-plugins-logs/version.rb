bc8fbb297055d67fc636e6e7500320cc43944faa
Jef Spaleta <jef@sensu.io>
2018-09-30 16:45:19 -0800

    Breaking change requires major version bump

--- lib/sensu-plugins-logs/version.rb
+++ lib/sensu-plugins-logs/version.rb
@@ -1,8 +1,8 @@
 module SensuPluginsLogs
   module Version
-    MAJOR = 1
-    MINOR = 3
-    PATCH = 3
+    MAJOR = 2
+    MINOR = 0
+    PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
